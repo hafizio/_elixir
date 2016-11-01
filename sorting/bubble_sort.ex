1b79aba20acf96a79ff95e507fbfcdb38064cb38
Hafiz Khairudin <ibnukamy@gmail.com>
2016-11-01 11:08:20 +0800

    Add note for bubble sort

--- sorting/bubble_sort.ex
+++ sorting/bubble_sort.ex
@@ -1,4 +1,18 @@
 defmodule BubbleSort do
+  @moduledoc """
+  Video: https://www.youtube.com/watch?v=8Kp-8OGwphY
+
+  One of the oldest sorting algorithms is Bubble sort the idea behind the sort is to make repeated passes through the list from beginning to end, comparing adjacent elements and swapping any that are out of order. After the first pass, the largest element will have been moved to the end of the list; after the second pass, the second largest will have been moved to the penultimate position; and so forth. The idea is that large values "bubble up" to the top of the list on each pass.
+
+  It should be clear that the algorithms does exactly the same key comparisons no matter what the contents of the array, so we need only consider the basic complexity of the algorithm.
+
+  On the first pass through the data, every element in the array but the first is compared with its predecessor, so n-1 comparisons are made. On the next pass, one less comparison is made, so n-2 comparisons are made. This continues until the last pass, where only one comparison is made. The total number of comparisons is thus given by the following summation.
+
+  C(n) = ∑i=1 to n-1i = n(n-1)/2
+  Clearly, n(n-1)/2 ∈ O(n2)
+
+  Bubble sort is not very fast. Various suggestions have been made to improve it. For example, a Boolean variable can be set to false at the beginning of each pass through the list and set to true whenever a swap is made. If the flag is false when the pass is completed, then no swaps were done and the array is sorted, so the algorithm can halt. This gives exactly the same worst case complexity, but a best case complexity of only n. The average case complexity is still in O(n2), however, so this is not much of an improvement.
+  """
 
   @doc """
   Reverse a list, when first parameter is a list
